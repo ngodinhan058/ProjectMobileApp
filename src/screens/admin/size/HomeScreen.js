7069841e415a693da0155b109e831416725d0935
ngodinhan058 <dinhanngo058@gmail.com>
2024-11-13 15:53:24 +0700

    complete crud size

---
+++ src/screens/admin/size/HomeScreen.js
@@ -0,0 +1,170 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Image, Pressable, ActivityIndicator } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from 'axios';
+import { BASE_URL } from '../../api/config';
+
+const HomeAdminScreen = ({ navigation }) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [SizeAll, setSizeAll] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const apiUrl = `${BASE_URL}product-sizes/category`;
+        axios.get(apiUrl)
+            .then(response => {
+                const sizeData = response.data.data;
+                setSizeAll(sizeData);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    const renderSizeChildren = (children) => {
+        if (!children || children.length === 0) return null; // Dừng nếu không có SizeChildren
+
+        return (
+            <FlatList
+                data={children}
+                renderItem={renderProduct}
+                keyExtractor={(item) => item.productSizeId}
+                style={styles.childList}
+            />
+        );
+    };
+
+    const renderProduct = ({ item }) => (
+        <View>
+            <TouchableOpacity
+                style={styles.productItem}
+                onPress={() => navigation.navigate('DetailSizeScreen', {
+                    id: item.productSizeId,
+                    name: item.productSizeName,
+                    type: item.productSizeType,
+                })}
+            >
+                <View style={{ width: 50, height: 50, backgroundColor: `${item.productSizeName}`, borderRadius: 50 }} ></View>
+                <View style={styles.productDetails}>
+                    <Text style={{
+                        fontSize: 16,
+                        fontWeight: 'bold',
+                        color: `${item.productSizeName}`,
+                        marginLeft: 20
+                        
+                    }}>{item.productSizeName}</Text>
+                </View>
+                <Pressable>
+                    <Icon name="angle-right" size={25} color="#000" />
+                </Pressable>
+            </TouchableOpacity>
+
+            {/* Hiển thị SizeChildren nếu có */}
+            {renderSizeChildren(item.SizeChildren)}
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            {/* Header */}
+            <View style={styles.header}>
+                <View style={styles.welcomeContainer}>
+                    <Text style={styles.welcomeText}>Hi Admin!</Text>
+                    <Text style={styles.subtitleText}>Welcome back to your panel.</Text>
+                </View>
+                <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
+                    <Image source={require('../../../assets/right_from_bracket.png')} style={{ width: 30, height: 30, marginLeft: 115 }} />
+                </TouchableOpacity>
+            </View>
+            {/* Product List */}
+            <FlatList
+                data={SizeAll}
+                renderItem={renderProduct}
+                keyExtractor={(item) => item.productSizeId}
+                style={styles.productList}
+            />
+
+            {/* Add Button */}
+            <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('AddSizeScreen')}>
+                <Text style={styles.addButtonText}>+</Text>
+            </TouchableOpacity>
+            {isLoading && (
+                <View style={styles.overlay}>
+                    <ActivityIndicator size="large" color="#3669c9" />
+                </View>
+            )}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    overlay: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1,
+    },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 30,
+    },
+    welcomeText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    subtitleText: {
+        fontSize: 16,
+        color: '#666',
+    },
+    productList: {
+        flex: 1,
+    },
+    productItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#eee',
+        borderWidth: 2,
+        padding: 20,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    productIcon: {
+        width: 55,
+        height: 55,
+        marginLeft: 5,
+        marginTop: 5,
+    },
+    productDetails: {
+        flex: 1,
+    },
+
+    childList: {
+        paddingLeft: 20, // Thêm khoảng cách cho danh sách con
+    },
+    addButton: {
+        position: 'absolute',
+        bottom: 30,
+        right: 30,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        backgroundColor: '#3669c9',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    addButtonText: {
+        fontSize: 40,
+        color: '#fff',
+    },
+});
+
+export default HomeAdminScreen;
