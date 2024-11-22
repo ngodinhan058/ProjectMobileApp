24e8644d3e781f4e6aece3c7d355410281541c12
ngodinhan058 <dinhanngo058@gmail.com>
2024-11-22 11:48:16 +0700

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import Icon from 'react-native-vector-icons/Ionicons';
-import React, { useEffect, useRef, useCallback } from 'react';
-import { View, TouchableOpacity, StyleSheet, Text, Easing } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, TouchableOpacity, StyleSheet, Text, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Sử dụng React.memo để tránh render lại nếu props không thay đổi
 const Footer = React.memo(({ state, descriptors, navigation, isVisible }) => {
@@ -11,6 +12,27 @@ const Footer = React.memo(({ state, descriptors, navigation, isVisible }) => {
     },
     [navigation]
   );
+
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        // Lấy dữ liệu từ AsyncStorage
+        const userInfoString = await AsyncStorage.getItem('userInfo');
+
+        // Nếu có dữ liệu thì parse nó thành JSON
+        if (userInfoString) {
+          const userInfoData = JSON.parse(userInfoString);
+          setUserInfo(userInfoData); // Lưu vào state
+        }
+      } catch (error) {
+        console.error('Error fetching user info from AsyncStorage:', error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
 
   return (
     <View style={styles.tabBarContainer}>
@@ -33,8 +55,22 @@ const Footer = React.memo(({ state, descriptors, navigation, isVisible }) => {
             onPress={() => handlePress(route.name)}
             style={styles.tabButton}
           >
-            <Icon name={iconName} size={24} color={isFocused ? '#3669c9' : '#999'} />
-            <Text style={{color: isFocused ? '#3669c9' : '#999', fontSize: 12,}}>{route.name}</Text>
+            {userInfo ? (
+              <>
+                
+                <View style={{ borderWidth: 2, borderColor: '#3669c9', width: 30, height: 30, borderRadius: 30}}>
+                  <Image source={{ uri: userInfo?.userImagePath }} style={{ width: 24, height: 24, borderRadius: 24 }}/>
+                </View>
+                <Text style={{ color: isFocused ? '#3669c9' : '#999', fontSize: 12 }}>{userInfo?.userFirstName} {userInfo?.userLastName}</Text>
+              </>
+            ) : (
+              <>
+                <Icon name={iconName} size={24} color={isFocused ? '#3669c9' : '#999'} />
+                <Text style={{ color: isFocused ? '#3669c9' : '#999', fontSize: 12 }}>{route.name}</Text>
+              </>
+            )}
+
+
           </TouchableOpacity>
         );
       })}
@@ -52,7 +88,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: 60,
   },
-  
+
   tabButton: {
     alignItems: 'center',
     justifyContent: 'center',
